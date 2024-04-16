d77e421d629e27a6a0561bb8f8a0e44be0a02e80
Irfan <irfannisho8571@gmail.com>
2024-04-16 12:16:31 +0600

    Add: user session at navbar

--- auth-sample/app/components/navbar/navbar.js
+++ auth-sample/app/components/navbar/navbar.js
@@ -1,23 +1,43 @@
+"use client"
+
 import Link from "next/link";
 import React from "react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Navbar = () => {
+    const { data: session } = useSession();
+    console.log( "Here  ",  { session });  
  
-  return (
-    <div className="bg-gradient-to-b from-cyan-50 to-cyan-200 px-8 py-3 flex gap-7">
-      <Link className="text-sky-600 hover:text-sky-700 font-extrabold text-xl" href={"/"}>
-          Home
-      </Link>
+    return (
+        <div className="bg-gradient-to-b from-cyan-50 to-cyan-200 px-8 py-3 flex gap-7">
+                <Link className="text-sky-600 hover:text-sky-700 font-extrabold text-xl" href={"/"}>
+                    Home
+                </Link>
 
-      <Link className="text-sky-600 hover:text-sky-700 font-extrabold text-xl" href={"/admin"}>
-          Admin
-      </Link>
-      <Link className="text-sky-600 hover:text-sky-700 font-extrabold text-xl" href={"/user"}>
-          User 
-      </Link>
-     
-    </div>
-  );
+                <Link className="text-sky-600 hover:text-sky-700 font-extrabold text-xl" href={"/admin"}>
+                    Admin
+                </Link>
+                <Link className="text-sky-600 hover:text-sky-700 font-extrabold text-xl" href={"/user"}>
+                    User 
+                </Link>
+
+                <div className="ml-auto flex gap-2">
+                    {session?.user ? (
+                    <>
+                        <p className="text-sky-600"> {session.user.name}</p>
+                        <button className="text-red-600  text-xl" onClick={() => signOut()}>
+                             Logout
+                        </button>
+                    </>
+                    ) : (
+                    <button className="text-sky-600 font-[500] text-xl" onClick={() => signIn()}>
+                        Login
+                    </button>
+                    )}
+            </div>
+        
+        </div>
+    );
 };
 
 export default Navbar;
--- auth-sample/app/layout.js
+++ auth-sample/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/navbar";
+import { AuthProvider } from "./Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,8 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
        <body className="{inter.className} bg-gray-200">
-             <Navbar />
-             {children}
+            <AuthProvider>
+                  <Navbar /> 
+                  {children}
+            </AuthProvider>
        </body>
     </html>
   );
