634afe4717792cf66e453f838f1f0607bcf4c088
Irfan <irfannisho8571@gmail.com>
2024-04-16 13:08:32 +0600

    Add: nextauth setup

---
+++ auth-sample/app/api/auth/[...nextauth].js
@@ -0,0 +1,47 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import NextAuth from "next-auth";
+
+export const authOptions = { 
+ providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      async authorize(credentials) {
+        const { username, password } = credentials;
+        const res = await fetch("http://localhost:4444/auth/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username,
+            password,
+          }),
+        });
+
+        const user = await res.json();
+
+        console.log({ user });
+
+        if (res.ok && user) {
+          return user;
+        } else return null;
+      },
+    }),
+ ],
+
+ callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+    async session({ session, token }) {
+      session.user = token;
+      return session;
+    },
+ },
+
+ pages: {
+    signIn: "/auth/login",
+ },
+};
+
+export default NextAuth(authOptions);
