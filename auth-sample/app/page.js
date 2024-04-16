f9e3eaa4e92b051677bdef0a040d7ed88ca85b5b
Irfan <irfannisho8571@gmail.com>
2024-04-16 11:21:36 +0600

    Set up: next js sample project with authentication

---
+++ auth-sample/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Fragment } from "react";
+
+export default function Home() {
+  return (
+      <Fragment>
+          <h1 className="text-xl text-red-600 font-bold flex justify-center items-center mt-12 ">
+               Hello Next JS
+          </h1>
+      </Fragment>
+  );
+}
